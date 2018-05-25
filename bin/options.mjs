6c2fef8e9aee680c79dbc83339b9be7ec16cf2b9
Wildhoney <adam.timberlake@gmail.com>
2018-05-26 00:07:51 +0100

    Added rest spread and fixed glob format

--- bin/options.mjs
+++ bin/options.mjs
@@ -1,11 +1,12 @@
 export default {
     dirname: 'es_modules',
-    glob: '{*,*/**}.{js,jsx,mjs}',
+    glob: '{*,**/*}.{js,jsx,mjs}',
     config: {
         babel: {
             sourceType: 'module',
             plugins: [
-                '@babel/plugin-syntax-jsx', '@babel/plugin-syntax-dynamic-import'
+                '@babel/plugin-syntax-jsx', '@babel/plugin-syntax-dynamic-import',
+                '@babel/plugin-syntax-object-rest-spread'
             ]
         },
         lebab: ['let', 'arrow'],
